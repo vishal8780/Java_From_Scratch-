462a36d77f9aba0a6ac8ca99a24089150eb24643
vishal <vishalkataka@gmail.com>
2023-03-02 14:01:20 +0530

    Palindrom

---
+++ StringInJava/src/Palindrom.java
@@ -0,0 +1,18 @@
+public class Palindrom {
+    public static void main(String[] args) {
+        String str1="MADAM";
+        String str2="";
+        for (int i=str1.length()-1;i>=0;i--)
+        {
+            str2 = str2 + str1.charAt(i);
+
+        }
+        if (str1.equals(str2))
+        {
+            System.out.println("palidrom");
+        }
+        else {
+            System.out.println(" not palindrom ");
+        }
+    }
+}
